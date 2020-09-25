ffe0e72ecb0aa6b91bd2c379be0881762ad0033c
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2020-09-25 17:04:39 +0200

    decode target name

--- 002.py
+++ 002.py
@@ -1,6 +1,7 @@
 
 import math
 import json
+import base64
 
 ## Read raw file and convert binary characters to ASCII
 raw = open('002-ppb.bin.log').read().split(' ')
@@ -28,6 +29,6 @@ stddev = math.sqrt((1./(len(contaminant_sum_series)-1)) * sum([(v-mean)**2 for v
 
 for i in range(len(contaminant_sum_series)):
     if abs(contaminant_sum_series[i] - mean) > stddev:
-        print(contaminant_sum_dates[i], contaminant_sum_series[i])
+        print(base64.b16decode(contaminant_sum_dates[i][-1]).decode())
 
 
